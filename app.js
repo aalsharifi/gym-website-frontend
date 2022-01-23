751451b3bf939d43183874d4642808b7dd8e9e34
aalsharifi <94931589+aalsharifi@users.noreply.github.com>
2022-01-22 22:27:15 -0500

    added animations

--- app.js
+++ app.js
@@ -11,3 +11,49 @@ const body = document.querySelector('body');
  }
 
  menu.addEventListener('click', mobileMenu);
+
+ //animations
+ gsap.registerPlugin(ScrollTrigger)
+
+ gsap.from('.animateHero', {
+    duration: 0.6, 
+    opacity: 0, 
+    y: -150, 
+    stagger: 0.3
+ });
+
+ gsap.from('.animateServices', {
+   ScrollTrigger: '.animateServices',
+   duration: 0.5, 
+   opacity: 1, 
+   x: -150, 
+   stagger: 0.12
+});
+
+gsap.from('.animateImg', {
+   ScrollTrigger: '.animateImg',
+   duration: 1.5, 
+   opacity: 0, 
+   x: -200, 
+});
+
+gsap.from('.animateMembership', {
+   ScrollTrigger: '.animateMembership',
+   duration: 1, 
+   opacity: 0, 
+   y: -150, 
+   stagger: 0.3,
+   delay: 0.5
+});
+
+gsap.from('.animateCard', {
+   ScrollTrigger: '.animateCard',
+   duration: 1, 
+   opacity: 0, 
+   y: -150, 
+   stagger: 0.1,
+   delay: 0.2
+});
+ 
+
+ 
